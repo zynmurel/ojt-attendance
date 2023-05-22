a5b4d5a4981141b0c29e026616bd468cab3a016a
Sonnykuds <bsonny962@gmail.com>
2023-05-22 10:48:58 +0800

    time rendered computation

--- src/pages/admin/currentIntern.jsx
+++ src/pages/admin/currentIntern.jsx
@@ -11,7 +11,40 @@ const { Meta } = Card;
 function CurrentIntern() {
   // Get_User Query Data
   const { data, loading, error } = useQuery(GET_INTERN);
-  console.log(data && data);
+
+  const computeHrMn = (intern) => {
+    let timeRendered;
+    intern.attendances.map((time) => {
+      const timeSplit = time.total_rendered
+        ? time.total_rendered?.split(":")
+        : ["0", "0"];
+      if (!timeRendered) {
+        timeRendered = timeSplit;
+      } else {
+        timeRendered = [
+          +timeRendered[0] + +timeSplit[0],
+          +timeRendered[1] + +timeSplit[1],
+        ];
+      }
+    });
+    let hr = +intern.hours_to_render;
+    let min = 0;
+    if (timeRendered?.[1] >= 60) {
+      const excessHr = timeRendered[1] / 60;
+      timeRendered = [
+        `${timeRendered[0] + excessHr}`,
+        `${timeRendered[1] % 60}`,
+      ];
+    }
+    if (timeRendered) {
+      hr = intern.hours_to_render - +timeRendered?.[0];
+      if (+timeRendered?.[1] > 0) {
+        hr = hr - 1;
+        min = 60 - timeRendered?.[1];
+      }
+    }
+    return [hr, min];
+  };
   return (
     <div className=" flex justify-start w-full flex-col">
       <Title
@@ -22,23 +55,26 @@ function CurrentIntern() {
       </Title>
       <div className="flex w-full  mt-10 flex-row flex-wrap justify-center md:justify-start">
         {data &&
-          data.ojt_attendance_user.map((intern) => (
-            <div className="flex flex-col m-3 w-64 h-96 bg-white hover:drop-shadow-2xl	 cursor-pointer hover:scale-105 ease-in duration-100 rounded">
-              <div className=" h-full rounded flex items-center  overflow-hidden">
-                <img
-                  src={intern.profile_pic}
-                  alt="Sample Images"
-                  className=" w-full rounded"
-                />
+          data.ojt_attendance_user.map((intern) => {
+            const time = computeHrMn(intern);
+            return (
+              <div className="flex flex-col m-3 w-64 h-96 bg-white hover:drop-shadow-2xl	 cursor-pointer hover:scale-105 ease-in duration-100 rounded">
+                <div className=" h-full rounded flex items-center  overflow-hidden">
+                  <img
+                    src={intern.profile_pic}
+                    alt="Sample Images"
+                    className=" w-full rounded"
+                  />
+                </div>
+                <div className=" w-full m-h-24 self-end p-5">
+                  <p className=" text-sm my-0 ">{intern.first_name}</p>
+                  <p className=" m-0 text-sm">
+                    {`Hours to render: ${time[0]}hrs ${time[1]}mins`}
+                  </p>
+                </div>
               </div>
-              <div className=" w-full m-h-24 self-end p-5">
-                <p className=" text-sm my-0 ">{intern.first_name}</p>
-                <p className=" m-0 text-sm">
-                  Hours to render: {intern.hours_to_render}
-                </p>
-              </div>
-            </div>
-          ))}
+            );
+          })}
       </div>
     </div>
   );
